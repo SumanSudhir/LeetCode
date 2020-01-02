fcf8f75f72f162009702a3910d423ba3c01b888a
SumanSudhir <sudhiriitb27@gmail.com>
2020-01-02 22:07:07 +0530

    code done

--- 0004.MedianofTwoSortedArrays/main.cpp
+++ 0004.MedianofTwoSortedArrays/main.cpp
@@ -3,49 +3,52 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int startX,endX, positionX, positionY, minRightX, minRightY, maxLeftX, maxLeftY;
-
+        int startX,endX, partitionX, partitionY, minRightX, minRightY, maxLeftX, maxLeftY;
+        vector<int> arrayX;
+        vector<int> arrayY;
         if(n1<=n2){
             startX = 0;
             endX = n1;
-            vector<int> arrayX = nums1;
-            vector<int> arrayY = nums2;
+            arrayX = nums1;
+            arrayY = nums2;
         }
         else{
             startX = 0;
             endX = n2;
-            vector<int> arrayX = nums2;
-            vector<int> arrayY = nums1;
+            arrayX = nums2;
+            arrayY = nums1;
         }
 
         while(true){
-            partionX = (startX + endX)/2;
-            partionY = (n1+n2+1)/2 - partionX;
+            partitionX = (startX + endX)/2;
+            partitionY = (n1+n2+1)/2 - partitionX;
 
             //For dealing with the boundary case
 
-            if(positionX == 0){
+            if(partitionX == 0){
                 maxLeftX = min(nums1[0], nums2[0]) - 1;
-                minRightX = arrayX[positionX];
+                minRightX = arrayX[partitionX];
             }
-            else if(partionX == (min(n1,n2))){
+            else if(partitionX == (min(n1,n2))){
                 minRightX = max(nums1[n1-1], nums2[n2-1]) + 1;
-                manRightX = arrayX[positionX - 1];
+                maxLeftX = arrayX[partitionX - 1];
             }
             else{
-                minRightX = arrayX[positionX];
-                maxRightX = arrayX[positionX - 1];
+                minRightX = arrayX[partitionX];
+                maxLeftX = arrayX[partitionX - 1];
             }
 
-            maxLeftY = arrayY[positionY-1];
-            minRightY = arrayY[positionY];
+            maxLeftY = arrayY[partitionY-1];
+            minRightY = arrayY[partitionY];
 
             if((maxLeftX <= minRightY) && (maxLeftY <= minRightX)){
                 if((n1+n2)%2 == 0){
                     return max(maxLeftX, maxLeftY);
+                    break;
                 }
                 else{
                     return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2.0;
+                    break;
                 }
             }
             else if(maxLeftX > minRightY){
