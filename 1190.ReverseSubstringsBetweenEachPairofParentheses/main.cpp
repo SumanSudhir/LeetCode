5013dd193e8e5ede6b2fa21e99b5ce872b227323
SumanSudhir <sudhiriitb27@gmail.com>
2020-01-09 20:13:50 +0530

    submitted

--- 1190.ReverseSubstringsBetweenEachPairofParentheses/main.cpp
+++ 1190.ReverseSubstringsBetweenEachPairofParentheses/main.cpp
@@ -2,33 +2,29 @@ class Solution {
 public:
     string reverseParentheses(string s) {
         string output;
-        queue<int>index;
-        int count = 1;
+        stack<int>index_open;
+        int count = 0;
+        char temp;
 
         for(int i=0;i<s.length();i++){
-            if((s[i] == '(') || (s[i] == ')')){
-                index.push(i);
+            if(s[i] == '('){
+                index_open.push(i-count);
+                count++;
+            }
+            else if(s[i] == ')'){
+                count++;
+                int start = index_open.top();
+                index_open.pop();
+                int end  = i - count;
+                for(int i=start;i<(start + end + 1)/2;i++){
+                    temp = output[i];
+                    output[i] = output[end + start - i];
+                    output[end + start - i] = temp;
+                }
             }
             else{
-                output.push_back(s[i]); 
+                output.push_back(s[i]);
             }
-        }
-
-        while(true){
-            int start = index.front();
-            index.pop();
-            if(index.empty()){
-                break;
-            }
-            int close = index.front();
-
-            if(count%2 != 0){
-                for(int i=start+1; i<close; i++){
-                    output[i-count] = s[close + start - i];
-                }                               
-            }
-
-            count++;
         }
 
         return output;
