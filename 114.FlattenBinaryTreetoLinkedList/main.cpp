e7e7f4dc22d7f2c30f5547753fcbfd93ec8ce680
SumanSudhir <sudhiriitb27@gmail.com>
2020-01-11 22:56:08 +0530

    code submitted

--- 114.FlattenBinaryTreetoLinkedList/main.cpp
+++ 114.FlattenBinaryTreetoLinkedList/main.cpp
@@ -7,18 +7,27 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if(root == NULL){
-            return;
-        }
 
-        TreeNode* tree = new TreeNode(root->val);
-        tree->right = root->left;
-        tree->right->val = root->left->val;
-        flatten(root->left);
-        flatten(root->right);
-        root = tree;
+        while(root != NULL){
+            if(root->left != NULL && root->right != NULL){
+                TreeNode* temp = root->left;
+                while(temp->right != NULL){
+                    temp = temp->right;
+                }
+                temp->right = root->right;              //Find the right most leaf of root's left
+                                                        // and set leaf's right = root's right
+            }
+
+            if(root->left != NULL){                     //Set root's right = root's left if
+                root->right = root->left;               // root's left exists
+            }
+
+            root->left = NULL;
+            root = root->right;                         //Move the root's left branch, if no left
+        }                                                //branch exists, then move root' right branch
     }
 };
