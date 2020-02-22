121a318666f4d048f98e041e953a9a6adca81bc0
SumanSudhir <sudhiriitb27@gmail.com>
2020-02-22 23:17:24 +0530

    accepted

--- 0031.NextPermutation/main.cpp
+++ 0031.NextPermutation/main.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         bool found = false;
-        for(int i=nums.size()-1,i>0;i--){
-            for(int j=i-1;j>=0;j--){
-                if(nums[i] > nums[j]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    found = true;
-                    break;
+        for(int i=nums.size()-1;i>0;i--){
+            if(nums[i] > nums[i-1]){
+                int temp = nums[i-1];
+                int diff = nums[i]-temp;
+                int index = i;
+                for(int j=i;j<nums.size();j++){
+                    if((nums[j]-temp) > 0 && (nums[j]-temp) <= diff){
+                        index = j;
+                        diff = nums[j]-temp;
+                    }
                 }
+                nums[i-1] = nums[index];
+                nums[index] = temp;
+                found = true;
+                reverse(nums.begin()+i,nums.end());
+                break;
             }
         }
         if(!found) reverse(nums.begin(),nums.end());
