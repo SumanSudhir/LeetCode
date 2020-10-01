feb7f7c3b310a6c9769fbb8687f6896fc09368e0
SumanSudhir <sudhiriitb27@gmail.com>
2020-10-01 10:18:50 +0530

    accepted

---
+++ 1486.XOROperationinanArray/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+
+        // vector<int> nums;
+        int output = 0;
+
+        for(int i=0;i<n;i++){
+            output = output^(start + 2*i);
+        }
+
+
+        return output;
+
+    }
+};
